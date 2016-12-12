0109a89100b2adf7618f70947a1b8cbcfbc178a4
Diogo Canut <diogocanut@hotmail.com>
2016-12-12 03:13:31 -0200

    gogo

--- trabalhosbd.py
+++ trabalhosbd.py
@@ -1,7 +1,8 @@
 import psycopg2
 
-conn = psycopg2.connect(host='localhost', user='postgres', password='postgres',dbname='trabalhosbd')
+conn = psycopg2.connect(host='localhost', user='postgres', password='postgres',dbname='xxx2')
 cur = conn.cursor()
+curr = conn.cursor()
 
 
 def cria_tabelas():
@@ -10,53 +11,56 @@ def cria_tabelas():
 	cur.execute('CREATE TABLE unidade_academica (sigla varchar, nome varchar PRIMARY KEY, area_de_conhecimento varchar);')
 
 	cur.execute('CREATE TABLE pessoa (nome varchar NOT NULL, CPF varchar PRIMARY KEY, email_i varchar, email_s varchar, data_nascimento date, senha varchar);')
-	cur.execute('CREATE TABLE professores (SIAPE varchar, FOREIGN KEY (nome) REFERENCES unidade_academica(nome), regime_de_trabalho int) INHERITS (pessoa);')
+	cur.execute('CREATE TABLE professores (SIAPE varchar, nome_u varchar, FOREIGN KEY (nome_u) REFERENCES unidade_academica(nome), regime_de_trabalho int) INHERITS (pessoa);')
 	cur.execute('CREATE TABLE alunos (nro_matricula int, curso varchar) INHERITS (pessoa);')
-	cur.execute('CREATE TABLE tecnicos (SIAPE varchar, FOREIGN KEY (nome) REFERENCES unidade_administrativa(nome)) INHERITS (pessoa);')
-	cur.execute('CREATE TABLE curso (sigla_c varchar PRIMARY KEY, nome varchar NOT NULL, FOREIGN KEY (nome) REFERENCES unidade_academica(nome));')
+	cur.execute('CREATE TABLE tecnicos (SIAPE varchar, nome_a varchar, FOREIGN KEY (nome_a) REFERENCES unidade_administrativa(nome)) INHERITS (pessoa);')
+	cur.execute('CREATE TABLE curso (sigla_c varchar PRIMARY KEY, nome_a varchar, nome varchar NOT NULL, FOREIGN KEY (nome) REFERENCES unidade_academica(nome));')
 	cur.execute('CREATE TABLE tercerizados (empresa varchar, setor_atuacao varchar) INHERITS (pessoa);')
 
+	
+	cur.execute('CREATE TABLE formulario (ID int PRIMARY KEY,nome_form varchar, nome_criador varchar, inicio date, fim date);')
 
-	cur.execute('CREATE TABLE respostas (ID int PRIMARY KEY, texto varchar, sim_nao boolean);')
-	cur.execute('CREATE TABLE questoes (descricao varchar, ID int PRIMARY KEY, FOREIGN KEY (ID) REFERENCES respostas(ID));')
+	cur.execute('CREATE TABLE questoes (descricao varchar, ID_q int PRIMARY KEY,ID_form int, FOREIGN KEY (ID_form) REFERENCES formulario(ID));')
+	cur.execute('CREATE TABLE respostas (texto varchar , sim_nao boolean, ID int, FOREIGN KEY (ID) REFERENCES questoes(ID_q));')
 
-	cur.execute('CREATE TABLE formulario (ID int PRIMARY KEY, FOREIGN KEY (ID) REFERENCES questoes(ID), inicio date, fim date);')
+	
 	conn.commit()
 
 def povoamento_inicial():
-	cur.execute('INSERT INTO unidade_academica VALUES ("facom","faculdade de computacao","exatas");')
-	cur.execute('INSERT INTO unidade_academica VALUES ("famat","faculdade de matematica","exatas");')
-	cur.execute('INSERT INTO unidade_academica VALUES ("fagen","faculdade de gestao de negocios","adminsitrativa");')
-	cur.execute('INSERT INTO unidade_academica VALUES ("IH","instituto de historia","humanas");')
-	cur.execute('INSERT INTO unidade_academica VALUES ("ifilo","instituto de filosofia","humanas");')
-	cur.execute('INSERT INTO unidade_academica VALUES ("ig","instituto de geografia","humanas");')
-	cur.execute('INSERT INTO unidade_academica VALUES ("iciag","instituto de ciencias agrarias","agrarias");')
-	cur.execute('INSERT INTO unidade_academica VALUES ("feciv","faculdade de engenharia civil","exatas");')
+	cur.execute('INSERT INTO unidade_academica VALUES (%s,%s,%s);' ,("facom","faculdade de computacao","exatas",))
+	cur.execute('INSERT INTO unidade_academica VALUES (%s,%s,%s);' ,("famat","faculdade de matematica","exatas",))
+	cur.execute('INSERT INTO unidade_academica VALUES (%s,%s,%s);' ,("fagen","faculdade de gestao de negocios","adminsitrativa",))
+	cur.execute('INSERT INTO unidade_academica VALUES (%s,%s,%s);' ,("IH","instituto de historia","humanas",))
+	cur.execute('INSERT INTO unidade_academica VALUES (%s,%s,%s);' ,("ifilo","instituto de filosofia","humanas",))
+	cur.execute('INSERT INTO unidade_academica VALUES (%s,%s,%s);' ,("ig","instituto de geografia","humanas",))
+	cur.execute('INSERT INTO unidade_academica VALUES (%s,%s,%s);' ,("iciag","instituto de ciencias agrarias","agrarias",))
+	cur.execute('INSERT INTO unidade_academica VALUES (%s,%s,%s);' ,("feciv","faculdade de engenharia civil","exatas",))
 
-	cur.execute('INSERT INTO unidade_administrativa VALUES ("stam","unidade santa monica");')
-	cur.execute('INSERT INTO unidade_administrativa VALUES ("ed","unidade engenheiro diniz");')
-	cur.execute('INSERT INTO unidade_administrativa VALUES ("fd","fazenda do gloria");')
-	cur.execute('INSERT INTO unidade_administrativa VALUES ("umu","campus umuarama");')
-	cur.execute('INSERT INTO unidade_administrativa VALUES ("educa","campus educacao fisica");')
-	cur.execute('INSERT INTO unidade_administrativa VALUES ("cp","campos pontal");')
-	cur.execute('INSERT INTO unidade_administrativa VALUES ("santa","campus santa monica");')
+	cur.execute('INSERT INTO unidade_administrativa VALUES (%s,%s);' ,("stam","unidade santa monica",))
+	cur.execute('INSERT INTO unidade_administrativa VALUES (%s,%s);' ,("ed","unidade engenheiro diniz",))
+	cur.execute('INSERT INTO unidade_administrativa VALUES (%s,%s);' ,("fd","fazenda do gloria",))
+	cur.execute('INSERT INTO unidade_administrativa VALUES (%s,%s);' ,("umu","campus umuarama",))
+	cur.execute('INSERT INTO unidade_administrativa VALUES (%s,%s);' ,("educa","campus educacao fisica",))
+	cur.execute('INSERT INTO unidade_administrativa VALUES (%s,%s);' ,("cp","campos pontal",))
+	cur.execute('INSERT INTO unidade_administrativa VALUES (%s,%s);' ,("santa","campus santa monica",))
 
-	cur.execute('INSERT INTO cursos ("CC","ciencia da computacao","faculdade de computacao");')
-	cur.execute('INSERT INTO cursos ("MAT","matematica","faculdade de matematica");')
-	cur.execute('INSERT INTO cursos ("ADM","administracao","faculdade de gestao de negocios");')
-	cur.execute('INSERT INTO cursos ("HIST","historia","instituto de historia");')
-	cur.execute('INSERT INTO cursos ("FILO","filosofia","instituto de filosofia");')
-	cur.execute('INSERT INTO cursos ("IG","geografia","instituto de geografia");')
-	cur.execute('INSERT INTO cursos ("AGRO","agronomia","instituto de ciencias agrarias");')
-	cur.execute('INSERT INTO cursos ("CIV","engenharia civil","faculdade de engenharia civil");')
+	cur.execute('INSERT INTO curso VALUES (%s,%s,%s);' ,("CC","ciencia da computacao","faculdade de computacao",))
+	cur.execute('INSERT INTO curso VALUES (%s,%s,%s);' ,("MAT","matematica","faculdade de matematica",))
+	cur.execute('INSERT INTO curso VALUES (%s,%s,%s);' ,("ADM","administracao","faculdade de gestao de negocios",))
+	cur.execute('INSERT INTO curso VALUES (%s,%s,%s);' ,("HIST","historia","instituto de historia",))
+	cur.execute('INSERT INTO curso VALUES (%s,%s,%s);' ,("FILO","filosofia","instituto de filosofia",))
+	cur.execute('INSERT INTO curso VALUES (%s,%s,%s);' ,("IG","geografia","instituto de geografia",))
+	cur.execute('INSERT INTO curso VALUES (%s,%s,%s);' ,("AGRO","agronomia","instituto de ciencias agrarias",))
+	cur.execute('INSERT INTO curso VALUES (%s,%s,%s);' ,("CIV","engenharia civil","faculdade de engenharia civil",))
 
-	cur.execute('INSERT INTO cursos ("CIV","engenharia civil","faculdade de engenharia civil");')
-	cur.execute('INSERT INTO cursos ("CIV","engenharia civil","faculdade de engenharia civil");')
-	cur.execute('INSERT INTO cursos ("CIV","engenharia civil","faculdade de engenharia civil");')
-	cur.execute('INSERT INTO cursos ("CIV","engenharia civil","faculdade de engenharia civil");')
-	cur.execute('INSERT INTO cursos ("CIV","engenharia civil","faculdade de engenharia civil");')
-	cur.execute('INSERT INTO cursos ("CIV","engenharia civil","faculdade de engenharia civil");')
 	
+	cur.execute('INSERT INTO formulario VALUES (%s,%s,%s,%s,%s);' ,(1,"Tráfico de drogas no campus","knut","01-12-2016","01-04-2017",))
+
+	cur.execute('INSERT INTO questoes VALUES (%s,%s,%s);' ,("Você é a favor da livre entrada da policia militar no campus universitário?(S/N)?",1,1,))
+	cur.execute('INSERT INTO questoes VALUES (%s,%s,%s);' ,("Você utiliza algum tipo de droga dentro do campus(Alcool/Cigarro/Ilegais)?",2,1,))
+	cur.execute('INSERT INTO questoes VALUES (%s,%s,%s);' ,("Você é a favor de novas medidas para impedir o tráfico de drogas no campus(S/N)?",3,1,))
+	cur.execute('INSERT INTO questoes VALUES (%s,%s,%s);' ,("Você considera a segurança atual do campus é satisfatória(S/N)?",4,1,))
+	conn.commit()
 
 
 
@@ -83,6 +87,7 @@ def nova_pessoa():
 		curso = input()
 		cur.execute("INSERT INTO alunos (nro_matricula,curso,cpf,nome,email_i,email_s,data_nascimento,senha) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", (matricula,curso,cpf,nome,emaili,emails,datanascimento,senha,))
 		conn.commit()
+		print("Criado com sucesso")
 
 	elif escolha == '2':
 		print('Digite seu SIAPE')
@@ -108,7 +113,47 @@ def pessoa_existente():
 	cur.execute('SELECT senha FROM pessoa WHERE cpf = %s', (cpf,))
 	x = cur.fetchone()
 	if senha == x[0]:
-		print('LOGADO')
+		return True
+	else:
+		return False
+
+def login():
+	while True:
+		print('1 - Criar novo usuário')
+		print('2 - Entrar em usuário existinte')
+		escolha = input()
+		if escolha == '1':
+			nova_pessoa()
+		elif escolha == '2':
+			if pessoa_existente():
+				break
+			else:
+				print('CPF ou senha não existentes')
+		elif escolha != '1' and escolha != '2':
+			print('Opcões são apenas 1 ou 2')
+
+def responder_form_existente():
+	cur.execute("SELECT * FROM formulario;")
+	for record in cur:
+		print("Nome do formulário: %s Código: %d" % (record[1],record[0]))
+	print("Digite o código do formulário em que deseja responder")
+	codigo = input()
+	cur.execute("SELECT descricao FROM questoes WHERE ID_form = %s", (codigo,))
+	for record in cur:
+		print(record[0])
+		resp = input()
+		if resp == 'S':
+			curr.execute("INSERT INTO respostas VALUES (%s,%s,%s)", ("sim","1",codigo,))
+			conn.commit()
+		elif resp == 'N':
+			curr.execute("INSERT INTO respostas VALUES (%s,%s,%s)", ("nao","0",codigo,))
+			conn.commit()
+		elif resp != 'N' and resp != 'S':
+			curr.execute("INSERT INTO respostas VALUES (%s,%s,%s)", (resp,"1",codigo,))
+			conn.commit()
+
+
+responder_form_existente()
 
 
 
@@ -119,5 +164,6 @@ def pessoa_existente():
 
 
 
-nova_pessoa()
+
+
 
